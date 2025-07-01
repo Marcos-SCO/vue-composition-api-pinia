57e989550dbcbedbe3322e59f645a79d9ca017ca
marcos-sco <marcos_sco@outlook.com>
2025-07-01 15:49:44 -0300

    feat: passing props and scoped styles playthrough

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   import { ref, reactive } from "vue";
   import tasks from "./utils/tasks";
 
-  const appName = "My new task manager";
+  import Task from "./components/Task.vue";
+
+  const appName = "Tasks Manager";
 
   const reactiveTasks = reactive(tasks);
 
@@ -17,10 +19,21 @@
       return;
     }
 
+    newTask.id = Math.max(...reactiveTasks.map((tasks) => tasks.id)) + 1;
+
     reactiveTasks.push(newTask);
 
-    // newTask = { completed: false, name: "", description: "" };
     newTask = { completed: false };
+  }
+
+  function handleToggleCompleted(taskId) {
+    reactiveTasks.forEach((task) => {
+      if (task.id === taskId) {
+        task.completed = !task.completed;
+
+        console.log(task);
+      }
+    });
   }
 </script>
 
@@ -46,14 +59,12 @@
     </div>
 
     <div class="tasks">
-      <div class="task" :key="taskIndex" v-for="(reactiveTask, taskIndex) in reactiveTasks">
-        <h3>{{ reactiveTask.name }}</h3>
-        <p>{{ reactiveTask.description }}</p>
-        <div class="task-check">
-          <input type="checkbox" :checked="reactiveTask.completed" />
-          <label> Done </label>
-        </div>
-      </div>
+      <Task
+        @toggleCompleted="handleToggleCompleted"
+        :reactiveTask="reactiveTask"
+        :key="taskIndex"
+        v-for="(reactiveTask, taskIndex) in reactiveTasks"
+      />
     </div>
 
     <div class="add-task">
@@ -131,80 +142,6 @@
 
     @media (max-width: 768px) {
       grid-template-columns: repeat(1, 1fr);
-    }
-  }
-
-  .task {
-    display: flex;
-    flex-direction: column;
-    background-color: var(--white-color);
-    color: var(--black-color);
-    padding: 20px;
-    border-radius: 12px;
-    position: relative;
-
-    h3 {
-      font-size: 20px;
-      font-weight: 700;
-      line-height: 21px;
-      letter-spacing: 0em;
-      text-align: left;
-    }
-
-    p {
-      margin-top: 24px;
-      margin-bottom: 12px;
-      font-size: 16px;
-      font-weight: 400;
-      line-height: 16px;
-      letter-spacing: 0em;
-      text-align: left;
-    }
-
-    .task-check {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: absolute;
-      bottom: 10px;
-      right: 10px;
-
-      label {
-        font-size: 13px;
-        font-weight: 400;
-        line-height: 16px;
-        letter-spacing: 0em;
-        text-align: left;
-        margin-left: 5px;
-        cursor: pointer;
-      }
-
-      input {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 18px;
-        height: 18px;
-        border-radius: 100%;
-        border: 0.77px solid #aeaeb2;
-        appearance: none;
-        cursor: pointer;
-
-        &:checked {
-          background-color: #0a7aff;
-          border-color: #0a7aff;
-
-          &::before {
-            content: "";
-            display: block;
-            width: 4.5px;
-            height: 9px;
-            border: solid white;
-            border-width: 0 2px 2px 0;
-            transform: rotate(45deg);
-          }
-        }
-      }
     }
   }
 
